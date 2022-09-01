1ff28b44a11627b39486a1255d51c490cb17b826
Mukesh A <90620152+Mukesh-A@users.noreply.github.com>
2022-09-01 13:45:38 +0530

    TO-DO Usestate

--- To-do list using states/src/App.js
+++ To-do list using states/src/App.js
@@ -2,23 +2,44 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+
+const getLocalStorageItem = ()=>{
+  let list = localStorage.getItem('list')
+  if(list)
+  {
+    return JSON.parse(localStorage.getItem('list'))
+  }
+  else{
+    return []
+  }
+}
+
 function App() {
   const [name,setName] = useState('');
-  const [list,setList] = useState([]);
-  const [isEditing, setEditing] = useState(false);
+  const [list,setList] = useState(getLocalStorageItem);
+  const [isEditing, setIsEditing] = useState(false);
   const [editId,setEditId]= useState(null)
   const [alert,setAlert] = useState({show:false,msg:'',type:''})
 
 
   const handleSubmit = (e)=>{
     e.preventDefault();
-    console.log("hello")
+    
     if(!name){
       //display alert
       showAlert(true,'danger','please enter the value')
     }
     else if(name && isEditing){
-      //edit
+      setList(list.map((item)=>{
+        if(item.id === editId){
+          return {...item,title: name}
+        }
+        return item
+      }))
+      setName('');
+      setEditId(null)
+      setIsEditing(false)
+      showAlert(true,'success','value changed')
     }
     else{
       //show list
@@ -43,6 +64,16 @@ function App() {
     setList(list.filter((item)=> item.id !== id))
   }
 
+  const editItem = (id)=>{
+    const specificItem = list.find((item)=> item.id === id);
+    setIsEditing(true)
+    setEditId(id)
+    setName(specificItem.title)
+  }
+  useEffect(()=>{
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
+
   return <section className='section-center'>
     <form className='grocery-form' onSubmit={handleSubmit}>
     {
@@ -55,7 +86,7 @@ function App() {
     </form>
     {list.length > 0 && (
     <div className="grocery-contain er">
-      <List items={list} removeItem={removeItem} />
+      <List items={list} removeItem={removeItem} editItem={editItem}/>
       <button className="clear-btn" onClick={clearList}>
         clear items
       </button>
